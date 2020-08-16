1439947c796da353f1398b503845152b203e56cd
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-08-16 14:07:27 -0500

    Fix a typo

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License'
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.8',
     ],
 )
