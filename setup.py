0244df6be47ed65910608074b24fd36b519a2676
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-08-16 13:55:39 -0500

    Get ready for library

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
-from setuptools import setup, find_packages
-
-setup(name="lmgtfyreborn", version="0.0.1", packages=find_packages())
+from distutils.core import setup
+setup(
+    name='lmgtfyreborn',
+    packages=['lmgtfyreborn'],
+    version='0.1',
+    license='WTFPL',
+    description='A library to get a Lmgfty url from a set of terms',
+    author='Diego Magdaleno',
+    author_email='diegomagdaleno@protonmail.com',
+    url='https://github.com/DiegoMagdaleno/imgtify-reborn',
+    download_url='https://github.com/DiegoMagdaleno/lmgtfyreborn/archive/0.1.tar.gz',
+    keywords=['lmgtfy'],
+    classifiers=[
+        'Development Status :: 5 - Production',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: WTFPL',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
