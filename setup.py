e1a1f35ef06c15db6dcbfab9df7cb170129202ed
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-08-16 13:56:21 -0500

    Update

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name='lmgtfyreborn',
     packages=['lmgtfyreborn'],
-    version='0.1',
+    version='0.2',
     license='WTFPL',
     description='A library to get a Lmgfty url from a set of terms',
     author='Diego Magdaleno',
     author_email='diegomagdaleno@protonmail.com',
     url='https://github.com/DiegoMagdaleno/imgtify-reborn',
-    download_url='https://github.com/DiegoMagdaleno/lmgtfyreborn/archive/0.1.tar.gz',
+    download_url='https://github.com/DiegoMagdaleno/lmgtfyreborn/archive/0.2.tar.gz',
     keywords=['lmgtfy'],
     classifiers=[
         'Development Status :: 5 - Production',
